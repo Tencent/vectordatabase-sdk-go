e2fd20631424ab2d5708b1dd3c654ead471c4efb
rogersqsliu <rogersqsliu@tencent.com>
2023-09-27 11:37:15 +0800

    add license

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,3 +1,21 @@
+// Copyright (C) 2023 Tencent Cloud.
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the vectordb-sdk-java), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is furnished
+// to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 package client
 
 import (
@@ -98,6 +116,7 @@ func (c *Client) Request(ctx context.Context, req, res interface{}) error {
 	auth := fmt.Sprintf("Bearer account=%s&api_key=%s", c.username, c.key)
 	request.Header.Add("Authorization", auth)
 	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Sdk-Version", SDKVersion)
 	response, err := c.cli.Do(request)
 	if err != nil {
 		return err
--- internal/client/version.go
+++ internal/client/version.go
@@ -16,18 +16,6 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 // SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-package entity
+package client
 
-type AliasResult struct {
-	AffectedCount int
-	Collection    string
-	Alias         []string
-}
-
-type SetAliasOption struct{}
-
-type DeleteAliasOption struct{}
-
-type DescribeAliasOption struct{}
-
-type ListAliasOption struct{}
+const SDKVersion = "v1.1.0"
