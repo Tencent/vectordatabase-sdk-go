ab7b1dec5194188b818afcafa9cd6431e6f7b512
rogersqsliu <rogersqsliu@tencent.com>
2024-10-25 14:19:02 +0800

    add windows build tag with jieba

---
+++ tcvdbtext/tokenizer/tokenizer_windows.go
@@ -0,0 +1,10 @@
+//go:build windows
+// +build windows
+
+package tokenizer
+
+import "errors"
+
+func NewJiebaTokenizer(params *TokenizerParams) (Tokenizer, error) {
+	return nil, errors.New("unsupported windows jieba tokenizer")
+}
